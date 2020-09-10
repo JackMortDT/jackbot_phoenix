0365086ae7c302b926fa7c7201da53467281ed11
Luisito <le.sastre@ebc.edu.mx>
2020-09-10 00:51:51 -0500

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,9 @@ use Mix.Config
 # before starting your production server.
 config :jackbot_phoenix, JackbotPhoenixWeb.Endpoint,
   url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "jackbot-phoenix.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- lib/jackbot_phoenix_web/endpoint.ex
+++ lib/jackbot_phoenix_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule JackbotPhoenixWeb.Endpoint do
   ]
 
   socket "/socket", JackbotPhoenixWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
